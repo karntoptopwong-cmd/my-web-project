22b9c3ed1bb3e72058656dded3c31300a4ce7176
raktherteisud-lang <raktherteisud@gmail.com>
2026-01-22 06:39:32 +0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,25 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const username = localStorage.getItem("loggedInUser");
+  const usernameDisplay = document.getElementById("usernameDisplay");
+  const editBtn = document.getElementById("editBtn");
+  const saveBtn = document.getElementById("saveBtn");
+  const inputs = document.querySelectorAll("#profileForm input");
+
   if (!username) {
     window.location.href = "index.html";
     return;
   }
 
-  const usernameDisplay = document.getElementById("usernameDisplay");
-  const editBtn = document.getElementById("editBtn");
-  const saveBtn = document.getElementById("saveBtn");
-  const form = document.getElementById("profileForm");
-
-  const inputs = form.querySelectorAll("input");
-
   usernameDisplay.textContent = username;
-
-  // โหลดข้อมูลที่เคยบันทึก
-  inputs.forEach(input => {
-    const saved = localStorage.getItem(input.id);
-    if (saved) input.value = saved;
-  });
 
   let editing = false;
 
@@ -31,33 +23,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     saveBtn.disabled = !editing;
-    editBtn.textContent = editing ? "✏️ Cancel" : "✏️ Edit";
+    editBtn.textContent = editing ? "✖ Cancel" : "✏️ Edit";
   });
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  document.getElementById("profileForm").addEventListener("input", () => {
+    const requiredFilled =
+      document.getElementById("fullname").value &&
+      document.getElementById("class").value &&
+      document.getElementById("number").value &&
+      document.getElementById("email").value;
 
-    // ตรวจ 4 ช่องบังคับ
-    if (
-      !fullname.value ||
-      !class.value ||
-      !number.value ||
-      !email.value
-    ) {
-      alert("กรุณากรอกข้อมูลให้ครบ");
-      return;
-    }
-
-    inputs.forEach(input => {
-      localStorage.setItem(input.id, input.value);
-      input.disabled = true;
-    });
-
-    saveBtn.disabled = true;
-    editing = false;
-    editBtn.textContent = "✏️ Edit";
-
-    alert("บันทึกข้อมูลเรียบร้อย");
+    saveBtn.disabled = !requiredFilled;
   });
 
 });
